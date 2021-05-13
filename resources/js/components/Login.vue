2dfc86f50346393fc01690bfd92c850b2e9f0978
mkjdy <markdy61@gmail.com>
2021-05-13 18:42:57 +0800

    Add Simple Vue component

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div> Hello {{ message }} </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "wellcome to vue js"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
