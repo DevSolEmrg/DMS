c36c56b6778a0096eb0fd2559837dba68077fb73
mkjdy <markdy61@gmail.com>
2021-05-16 13:27:48 +0800

    App Container Component

---
+++ resources/js/components/AppContainer.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-app>
+        <v-navigation-drawer app style="background-color:green">
+          <!-- -->
+          <div>hello</div>
+          <div class="text-center">
+            <v-dialog
+              v-model="dialog"
+              width="500"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="red lighten-2"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  Click Me
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class="headline grey lighten-2">
+                  Privacy Policy
+                </v-card-title>
+
+                <v-card-text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="primary"
+                    text
+                    @click="dialog = false"
+                  >
+                    I accept
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
+        </v-navigation-drawer>
+
+        <v-app-bar app style="background-color:blue">
+          <!-- -->
+          <div>hello</div>
+        </v-app-bar>
+
+        <!-- Sizes your content based upon application components -->
+        <v-main  style="background-color:pink">
+          <div>hello</div>
+
+          <!-- Provides the application the proper gutter -->
+          <v-container fluid  style="background-color:yellow">
+            <div>hello</div>
+
+            <!-- If using vue-router 
+            <router-view></router-view>-->
+          </v-container>
+        </v-main>
+
+        <v-footer app  style="background-color:violet">
+          <div>hello</div>
+          <!-- -->
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+  }
+</script>
