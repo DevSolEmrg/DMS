c54031551c8726d7c37ee8c2c9f2cd6444fab270
mkjdy <markdy61@gmail.com>
2021-05-16 13:27:31 +0800

    Add Vuetify, theme and app container

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,32 @@ require('./bootstrap');
 import Vue from 'vue';
 window.Vue = Vue;
 
-Vue.component('Login', require('./components/Login').default)
+//Vuetify Plugins
+import Vuetify from 'vuetify'
+Vue.use(Vuetify);
+//Theme Color
+import colors from 'vuetify/lib/util/colors'
+//End
+
+Vue.component('app-container', require('./components/AppContainer').default);
+Vue.component('login-page', require('./components/Login').default);
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    vuetify: new Vuetify({
+        theme: {
+            dark: false,
+            themes: {
+              light: {
+                primary: colors.blue,
+                secondary: colors.grey.darken1,
+                accent: colors.shades.black,
+                error: colors.red.accent3,
+              },
+              dark: {
+                primary: colors.blue.lighten3,
+              },
+            },
+        },
+    }),
 })
