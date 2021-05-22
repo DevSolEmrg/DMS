2dde04c678ded25c3f9b9b1455dfbd8d4b6900f3
mkjdy <markdy61@gmail.com>
2021-05-22 23:08:15 +0800

    Add not fount style

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -1,3 +1,27 @@
 <template>
-    <div>Not Found</div>
+    <v-app>
+        <v-main>
+            <v-container fluid fill-height>
+                <v-layout class="align-center justify-center">
+
+                    <v-btn color="primary" link outlined class="mr-2" @click="goBack">
+                        <v-icon left dark> mdi-keyboard-backspace </v-icon> Back
+                    </v-btn>
+                    <h3><span style="color:red">404 ERROR</span> - Page Not Found</h3>
+                        
+                </v-layout>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
+
+<script>
+
+export default {
+    methods: {
+        goBack() {
+            this.$router.push({ path: '/'});
+        }
+    }
+}
+</script>
