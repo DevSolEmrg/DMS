cf8b1058f03bb1e624b59dbfda7af7286f15219e
mkjdy <markdy61@gmail.com>
2021-05-22 23:07:50 +0800

    Add guest component

---
+++ resources/js/components/Guest.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-row>
+            <v-col>
+                GUEST AREA
+                <v-btn @click="loginPage">
+                    Go To Login Section
+                </v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        loginPage() {
+            this.$router.push({ name: "Login" })
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
