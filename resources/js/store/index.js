c6bdf1fe89ba3552026a77f95095d91a3e55a235
mkjdy <markdy61@gmail.com>
2021-08-29 17:04:11 +0800

    disable secure ls add reducer

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,8 +2,18 @@
 //import Vuex from 'vuex'
 
 import createPersistedState from "vuex-persistedstate";
-import SecureLS from "secure-ls";
-var ls = new SecureLS({ isCompression: false });
+//import SecureLS from "secure-ls";
+// class CustomSecureLS extends SecureLS {
+//     init() {
+//         this.utils.metaKey = `meta`;
+//         super.init();
+//     }
+// }
+// var ls = new CustomSecureLS({
+//     encodingType: 'aes',
+//     isCompression: false,
+//     encryptionSecret: `@210012~-%${Math.random().toString(36).slice(2)}%`,
+// });
 
 import users from "./modules/users";
 
@@ -18,11 +28,19 @@ const store = {
     plugins: [
         createPersistedState({
             //key: "secudata",
-            storage: {
-                getItem: (key) => ls.get(key),
-                setItem: (key, value) => ls.set(key, value),
-                removeItem: (key) => ls.remove(key),
+            // storage: {
+            //     getItem: (key) => ls.get(key),
+            //     setItem: (key, value) => ls.set(key, value),
+            //     removeItem: (key) => ls.remove(key),
+            // },
+            key: 'meta',
+            reducer (val) {
+                if (!val.users.auth) {
+                    return []
+                }
+                return val
             },
+            storage: localStorage
         }),
     ]
 }
